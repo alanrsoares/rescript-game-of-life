380b381817c1eda0d732ecd2fb1dd1b56b085e4a
Alan Soares <alanrsoars@gmail.com>
2020-04-22 00:31:38 +1200

    Updates

---
+++ precache-manifest.cc956e584b067dfc909dc50edf387947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8263a1afdda801b7a1b8b1239ff4ebb9",
+    "url": "/reason-game-of-life/index.html"
+  },
+  {
+    "revision": "9f93edf7e4dacbe6d833",
+    "url": "/reason-game-of-life/static/css/main.e94b00b1.chunk.css"
+  },
+  {
+    "revision": "c11aafb96e1686db5108",
+    "url": "/reason-game-of-life/static/js/2.3a0312e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reason-game-of-life/static/js/2.3a0312e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f93edf7e4dacbe6d833",
+    "url": "/reason-game-of-life/static/js/main.c3c25525.chunk.js"
+  },
+  {
+    "revision": "4576a3118e065885ba3d",
+    "url": "/reason-game-of-life/static/js/runtime-main.13ccb877.js"
+  }
+]);
